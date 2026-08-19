5c514285de565f44d9233f1f02195e4d02850ac0
iamteapot422 <iamteapot42@gmail.com>
2026-08-19 15:26:58 +0000

    cross1 final final

--- module3/cross1/client.c
+++ module3/cross1/client.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #include <signal.h>
+#include <stdint.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
 
-#define PORT 9000
+#define PORT 7000
+#define BUF_SIZE 2048
 
 static volatile sig_atomic_t g_stop = 0;
 
@@ -23,43 +28,135 @@ void install_signal_handlers()
     sigaction(SIGTERM, &sa, NULL);
 }
 
+unsigned int checksum_add(unsigned int sum, void* data, int len)
+{
+    unsigned short* buf = data;
+    for (; len > 1; len -= 2) sum += *buf++;
+    if (len == 1) sum += *(unsigned char*)buf;
+    return sum;
+}
+
+unsigned short checksum_done(unsigned int sum)
+{
+    while (sum >> 16) sum = (sum & 0xFFFF) + (sum >> 16);
+    return (unsigned short)~sum;
+}
+
+struct pseudo_header
+{
+    uint32_t src;
+    uint32_t dst;
+    uint8_t zero;
+    uint8_t protocol;
+    uint16_t len;
+};
+
+int build_packet(char* buf, uint32_t src_ip, uint16_t src_port, uint32_t dst_ip, uint16_t dst_port, const char* payload, int payload_len)
+{
+    struct iphdr* ip = (struct iphdr*)buf;
+    struct udphdr* udp = (struct udphdr*)(buf + sizeof(struct iphdr));
+    int udp_len = sizeof(struct udphdr) + payload_len;
+
+    memcpy(buf + sizeof(struct iphdr) + sizeof(struct udphdr), payload, payload_len);
+
+    udp->source = htons(src_port);
+    udp->dest = htons(dst_port);
+    udp->len = htons(udp_len);
+    udp->check = 0;
+
+    struct pseudo_header ph = { src_ip, dst_ip, 0, IPPROTO_UDP, udp->len };
+    unsigned int sum = checksum_add(0, &ph, sizeof(ph));
+    sum = checksum_add(sum, udp, udp_len);
+    udp->check = checksum_done(sum);
+
+    *ip = (struct iphdr){ .ihl = 5, .version = 4, .ttl = 64, .protocol = IPPROTO_UDP,
+                           .tot_len = htons(sizeof(struct iphdr) + udp_len),
+                           .saddr = src_ip, .daddr = dst_ip };
+    ip->check = checksum_done(checksum_add(0, ip, sizeof(struct iphdr)));
+
+    return sizeof(struct iphdr) + udp_len;
+}
+
+void send_payload(int sock, uint32_t own_ip, uint16_t own_port, uint32_t server_ip, char type, const char* text, int text_len)
+{
+    char payload[BUF_SIZE];
+    payload[0] = type;
+    if (text_len > 0) memcpy(payload + 1, text, text_len);
+
+    char packet[BUF_SIZE];
+    int plen = build_packet(packet, own_ip, own_port, server_ip, PORT, payload, 1 + text_len);
+
+    struct sockaddr_in dst = { .sin_family = AF_INET, .sin_addr.s_addr = server_ip };
+    sendto(sock, packet, plen, 0, (struct sockaddr*)&dst, sizeof(dst));
+}
+
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    if (argc < 4)
     {
-        printf("usage: %s <server ip>\n", argv[0]);
+        printf("usage: %s <own ip> <server ip> <own port>\n", argv[0]);
         return 1;
     }
 
     install_signal_handlers();
 
-    int fd = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in server = { .sin_family = AF_INET, .sin_port = htons(PORT) };
-    inet_pton(AF_INET, argv[1], &server.sin_addr);
+    uint32_t own_ip, server_ip;
+    inet_pton(AF_INET, argv[1], &own_ip);
+    inet_pton(AF_INET, argv[2], &server_ip);
+    uint16_t own_port = (uint16_t)atoi(argv[3]);
 
-    char line[1024];
+    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+    if (sock < 0)
+    {
+        perror("socket (run as root?)");
+        return 1;
+    }
+
+    int one = 1;
+    setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one));
+
+    char buf[BUF_SIZE];
 
     while (!g_stop)
     {
-        printf("> ");
-        fflush(stdout);
-
+        char line[256];
         if (fgets(line, sizeof(line), stdin) == NULL) break;
         line[strcspn(line, "\n")] = '\0';
         if (strlen(line) == 0) continue;
 
-        sendto(fd, line, strlen(line), 0, (struct sockaddr*)&server, sizeof(server));
+        send_payload(sock, own_ip, own_port, server_ip, 'M', line, strlen(line));
 
-        char reply[1100];
-        ssize_t n = recvfrom(fd, reply, sizeof(reply) - 1, 0, NULL, NULL);
-        if (n > 0)
+        while (!g_stop)
         {
-            reply[n] = '\0';
-            printf("%s\n", reply);
+            ssize_t n = recvfrom(sock, buf, sizeof(buf), 0, NULL, NULL);
+            if (n < 0)
+            {
+                if (errno == EINTR) break;
+                break;
+            }
+            if (n < (ssize_t)sizeof(struct iphdr)) continue;
+
+            struct iphdr* ip = (struct iphdr*)buf;
+            int ip_hlen = ip->ihl * 4;
+            if (n < ip_hlen + (ssize_t)sizeof(struct udphdr)) continue;
+
+            struct udphdr* udp = (struct udphdr*)(buf + ip_hlen);
+            if (ntohs(udp->dest) != own_port || ip->saddr != server_ip) continue;
+
+            int udp_len = ntohs(udp->len);
+            int payload_len = udp_len - sizeof(struct udphdr);
+            if (payload_len <= 0 || ip_hlen + udp_len > n) continue;
+
+            char* payload = buf + ip_hlen + sizeof(struct udphdr);
+            printf("server: %.*s\n", payload_len, payload);
+            fflush(stdout);
+            break;
         }
     }
 
-    sendto(fd, "CLOSE", 5, 0, (struct sockaddr*)&server, sizeof(server));
-    close(fd);
+    send_payload(sock, own_ip, own_port, server_ip, 'C', NULL, 0);
+    printf("client: sent close notification\n");
+
+    close(sock);
     return 0;
 }
--- module3/cross1/server.c
+++ module3/cross1/server.c
@@ -2,31 +2,81 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
 
-#define PORT 9000
+#define PORT 7000
+#define BUF_SIZE 2048
 #define MAX_CLIENTS 64
+
+unsigned int checksum_add(unsigned int sum, void* data, int len)
+{
+    unsigned short* buf = data;
+    for (; len > 1; len -= 2) sum += *buf++;
+    if (len == 1) sum += *(unsigned char*)buf;
+    return sum;
+}
+
+unsigned short checksum_done(unsigned int sum)
+{
+    while (sum >> 16) sum = (sum & 0xFFFF) + (sum >> 16);
+    return (unsigned short)~sum;
+}
+
+struct pseudo_header
+{
+    uint32_t src;
+    uint32_t dst;
+    uint8_t zero;
+    uint8_t protocol;
+    uint16_t len;
+};
+
+int build_packet(char* buf, uint32_t src_ip, uint16_t src_port, uint32_t dst_ip, uint16_t dst_port, const char* payload, int payload_len)
+{
+    struct iphdr* ip = (struct iphdr*)buf;
+    struct udphdr* udp = (struct udphdr*)(buf + sizeof(struct iphdr));
+    int udp_len = sizeof(struct udphdr) + payload_len;
+
+    memcpy(buf + sizeof(struct iphdr) + sizeof(struct udphdr), payload, payload_len);
+
+    udp->source = htons(src_port);
+    udp->dest = htons(dst_port);
+    udp->len = htons(udp_len);
+    udp->check = 0;
+
+    struct pseudo_header ph = { src_ip, dst_ip, 0, IPPROTO_UDP, udp->len };
+    unsigned int sum = checksum_add(0, &ph, sizeof(ph));
+    sum = checksum_add(sum, udp, udp_len);
+    udp->check = checksum_done(sum);
+
+    *ip = (struct iphdr){ .ihl = 5, .version = 4, .ttl = 64, .protocol = IPPROTO_UDP,
+                           .tot_len = htons(sizeof(struct iphdr) + udp_len),
+                           .saddr = src_ip, .daddr = dst_ip };
+    ip->check = checksum_done(checksum_add(0, ip, sizeof(struct iphdr)));
+
+    return sizeof(struct iphdr) + udp_len;
+}
 
 typedef struct
 {
-    struct in_addr ip;
+    uint32_t ip;
     uint16_t port;
     int counter;
-} client_entry;
+} client_state_t;
 
-client_entry clients[MAX_CLIENTS];
-int nclients = 0;
+static client_state_t clients[MAX_CLIENTS];
+static int nclients = 0;
 
-int find_client(struct sockaddr_in* addr)
+int find_client(uint32_t ip, uint16_t port)
 {
     for (int i = 0; i < nclients; i++)
     {
-        if (clients[i].ip.s_addr == addr->sin_addr.s_addr && clients[i].port == addr->sin_port)
-        {
-            return i;
-        }
+        if (clients[i].ip == ip && clients[i].port == port) return i;
     }
     return -1;
 }
@@ -37,57 +87,96 @@ void remove_client(int idx)
     nclients--;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    int fd = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in addr = { .sin_family = AF_INET, .sin_addr.s_addr = htonl(INADDR_ANY), .sin_port = htons(PORT) };
-    bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+    if (argc < 2)
+    {
+        printf("usage: %s <own ip>\n", argv[0]);
+        return 1;
+    }
+
+    uint32_t own_ip;
+    inet_pton(AF_INET, argv[1], &own_ip);
+
+    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+    if (sock < 0)
+    {
+        perror("socket (run as root?)");
+        return 1;
+    }
+
+    int one = 1;
+    setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one));
 
     printf("server: listening on udp port %d\n", PORT);
     fflush(stdout);
 
-    char buf[1024];
+    char buf[BUF_SIZE];
+    char packet[BUF_SIZE];
 
     while (1)
     {
-        struct sockaddr_in from;
-        socklen_t fromlen = sizeof(from);
-        ssize_t n = recvfrom(fd, buf, sizeof(buf) - 1, 0, (struct sockaddr*)&from, &fromlen);
-        if (n <= 0) continue;
-        buf[n] = '\0';
+        ssize_t n = recvfrom(sock, buf, sizeof(buf), 0, NULL, NULL);
+        if (n < (ssize_t)sizeof(struct iphdr)) continue;
 
-        int idx = find_client(&from);
+        struct iphdr* ip = (struct iphdr*)buf;
+        int ip_hlen = ip->ihl * 4;
+        if (n < ip_hlen + (ssize_t)sizeof(struct udphdr)) continue;
 
-        if (strcmp(buf, "CLOSE") == 0)
+        struct udphdr* udp = (struct udphdr*)(buf + ip_hlen);
+        if (ntohs(udp->dest) != PORT) continue;
+
+        int udp_len = ntohs(udp->len);
+        int payload_len = udp_len - sizeof(struct udphdr);
+        if (payload_len < 1 || ip_hlen + udp_len > n) continue;
+
+        char* payload = buf + ip_hlen + sizeof(struct udphdr);
+        char type = payload[0];
+        char* text = payload + 1;
+        int text_len = payload_len - 1;
+
+        uint32_t cip = ip->saddr;
+        uint16_t cport = ntohs(udp->source);
+
+        struct in_addr a;
+        a.s_addr = cip;
+
+        if (type == 'C')
         {
-            if (idx != -1)
+            int idx = find_client(cip, cport);
+            if (idx >= 0)
             {
-                printf("server: %s:%d closed, counter reset\n", inet_ntoa(from.sin_addr), ntohs(from.sin_port));
-                fflush(stdout);
                 remove_client(idx);
+                printf("server: %s:%d closed, counter reset\n", inet_ntoa(a), cport);
+                fflush(stdout);
             }
             continue;
         }
 
-        if (idx == -1)
+        if (type != 'M') continue;
+
+        int idx = find_client(cip, cport);
+        if (idx < 0)
         {
             if (nclients >= MAX_CLIENTS) continue;
             idx = nclients++;
-            clients[idx].ip = from.sin_addr;
-            clients[idx].port = from.sin_port;
+            clients[idx].ip = cip;
+            clients[idx].port = cport;
             clients[idx].counter = 0;
         }
-
         clients[idx].counter++;
 
-        char reply[1100];
-        int len = snprintf(reply, sizeof(reply), "%s %d", buf, clients[idx].counter);
-        sendto(fd, reply, len, 0, (struct sockaddr*)&from, fromlen);
+        char reply[BUF_SIZE];
+        int reply_len = snprintf(reply, sizeof(reply), "%.*s %d", text_len, text, clients[idx].counter);
 
-        printf("server: %s:%d -> \"%s\"\n", inet_ntoa(from.sin_addr), ntohs(from.sin_port), reply);
+        printf("server: %s:%d: %.*s -> \"%s\"\n", inet_ntoa(a), cport, text_len, text, reply);
         fflush(stdout);
+
+        int plen = build_packet(packet, own_ip, PORT, cip, cport, reply, reply_len);
+        struct sockaddr_in dst = { .sin_family = AF_INET, .sin_addr.s_addr = cip };
+        sendto(sock, packet, plen, 0, (struct sockaddr*)&dst, sizeof(dst));
     }
 
-    close(fd);
+    close(sock);
     return 0;
 }
