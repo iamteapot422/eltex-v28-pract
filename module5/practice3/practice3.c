79d4285d2cc82dc1a2e6bd1f726450536ae90fe4
iamteapot422 <iamteapot42@gmail.com>
2026-08-28 05:21:56 +0000

    update practice3

--- module5/practice3/practice3.c
+++ module5/practice3/practice3.c
@@ -1,5 +1,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/configfs.h>
 #include <linux/init.h>
 #include <linux/tty.h>
@@ -13,17 +14,20 @@
 #include <linux/sysfs.h>
 #include <linux/fs.h>
 #include <linux/string.h>
-#include <linux/tty.h>
 
-MODULE_DESCRIPTION("Управление светодиодами клавиатуры через sysfs");
+MODULE_DESCRIPTION("Blink the lights on your keyboard");
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("RickLater0");
+MODULE_AUTHOR("Bogdan Novitskiy");
 
-#define LOG_MODULE_NAME "leds blink module: "
-#define SYS_FNAME       "mleds.sys"
+#define LOG_MODULE_NAME "the_blinker: "
+#define SYS_FNAME       "practice3.sys"
 #define RESTORE_LEDS    0xFF
-#define SYS_FRIGHTS     0660
-#define BLINK_DELAY     (HZ)
+#define SYS_PERMS       0660
+#define LEDS_MASK_MAX   7u
+
+static unsigned int blink_delay_ms = 500;
+module_param(blink_delay_ms, uint, 0644);
+MODULE_PARM_DESC(blink_delay_ms, "Delay between LED toggles, in milliseconds");
 
 static struct kobject *leds_kobject;
 static unsigned int leds_status;
@@ -32,99 +36,107 @@ static struct tty_driver *tty_driver;
 static struct timer_list timer;
 
 static void blink(struct timer_list *ptr);
-static ssize_t sys_read (struct kobject *kobject, struct kobj_attribute *attr, char *buf);
+static ssize_t sys_read(struct kobject *kobject, struct kobj_attribute *attr, char *buf);
 static ssize_t sys_write(struct kobject *kobject, struct kobj_attribute *attr, const char *buf, size_t count);
-static int  __init sys_init (void);
-static void __exit sys_exit (void);
+static int  __init sys_init(void);
+static void __exit sys_exit(void);
 
-static struct kobj_attribute leds_attribute =__ATTR(
+static struct kobj_attribute leds_attribute = __ATTR(
         leds,
-        SYS_FRIGHTS,
+        SYS_PERMS,
         sys_read,
         sys_write
 );
 
-static int __init sys_init (void)
+static int __init sys_init(void)
 {
+        struct vc_data *vc_data;
+        struct tty_struct *tty_struct;
+        int error;
+
         printk(KERN_INFO LOG_MODULE_NAME "loading\n");
 
-        struct vc_data* vc_data = vc_cons[fg_console].d;
+        vc_data = vc_cons[fg_console].d;
         if (!vc_data)
                 return -EINVAL;
-        struct tty_struct* tty_struct = vc_data->port.tty;
+
+        tty_struct = vc_data->port.tty;
         if (!tty_struct)
                 return -EINVAL;
 
         tty_driver = tty_struct->driver;
-        if (!tty_struct)
+        if (!tty_driver)
                 return -EINVAL;
 
         leds_kobject = kobject_create_and_add(SYS_FNAME, kernel_kobj);
-        if(!leds_kobject)
+        if (!leds_kobject)
                 return -ENOMEM;
 
-        int error = 0;
-        if ((error = sysfs_create_file(leds_kobject, &leds_attribute.attr))) {
-                printk(KERN_INFO LOG_MODULE_NAME "failed to create the foo file in /sys/kernel/%s\n", SYS_FNAME);
+        error = sysfs_create_file(leds_kobject, &leds_attribute.attr);
+        if (error) {
+                printk(KERN_ERR LOG_MODULE_NAME "failed to create /sys/kernel/%s (%d)\n",
+                       SYS_FNAME, error);
+                kobject_put(leds_kobject);
+                return error;
         }
-        printk(KERN_INFO LOG_MODULE_NAME "tty driver - %s: %x %x\n", tty_driver->name, tty_driver->major, tty_driver->minor_start);
+
+        printk(KERN_INFO LOG_MODULE_NAME "tty driver - %s: %x %x\n",
+               tty_driver->name, tty_driver->major, tty_driver->minor_start);
 
         timer_setup(&timer, blink, 0);
-        timer.expires = jiffies + BLINK_DELAY;
+        timer.expires = jiffies + msecs_to_jiffies(blink_delay_ms);
         add_timer(&timer);
-        printk(KERN_INFO LOG_MODULE_NAME "initialized");
-        return error;
+
+        printk(KERN_INFO LOG_MODULE_NAME "initialized\n");
+        return 0;
 }
 
-static void __exit sys_exit (void)
+static void __exit sys_exit(void)
 {
         timer_delete_sync(&timer);
-        (tty_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,RESTORE_LEDS);
+
+        if (tty_driver && tty_driver->ops && tty_driver->ops->ioctl)
+                tty_driver->ops->ioctl(vc_cons[fg_console].d->port.tty, KDSETLED, RESTORE_LEDS);
+
         kobject_put(leds_kobject);
-        printk (KERN_INFO LOG_MODULE_NAME "uninitialized\n");
+        printk(KERN_INFO LOG_MODULE_NAME "uninitialized\n");
 }
 
 static ssize_t sys_read(struct kobject *kobject, struct kobj_attribute *attr, char *buf)
 {
-        printk(KERN_INFO LOG_MODULE_NAME "file output: %u", blink_mask);
         return sprintf(buf, "%u\n", blink_mask);
 }
- 
+
 static ssize_t sys_write(struct kobject *kobject, struct kobj_attribute *attr, const char *buf, size_t count)
 {
-        printk(KERN_INFO LOG_MODULE_NAME "tried to parse - %s", buf);
         unsigned int tmp = 0;
 
-        if(kstrtouint(buf, 0, &tmp) != 0)
+        if (kstrtouint(buf, 0, &tmp) != 0)
                 return -EINVAL;
-        if (tmp > 7)
+        if (tmp > LEDS_MASK_MAX)
                 return -EINVAL;
-        blink_mask = tmp;
 
-        printk(KERN_INFO LOG_MODULE_NAME "parsed - %u", blink_mask);
+        blink_mask = tmp;
+        printk(KERN_INFO LOG_MODULE_NAME "mask set to %u\n", blink_mask);
         return (ssize_t)count;
 }
 
-static void blink(struct timer_list *ptr) {
+static void blink(struct timer_list *ptr)
+{
         if (!tty_driver || !tty_driver->ops || !tty_driver->ops->ioctl) {
-                mod_timer(&timer, jiffies + BLINK_DELAY);
-                printk(KERN_WARNING LOG_MODULE_NAME "no tty");
-                return;
+                printk(KERN_WARNING LOG_MODULE_NAME "no tty\n");
+                mod_timer(&timer, jiffies + msecs_to_jiffies(blink_delay_ms));
         }
 
         if (blink_mask == 0) {
-                (tty_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED, RESTORE_LEDS);
+                tty_driver->ops->ioctl(vc_cons[fg_console].d->port.tty, KDSETLED, RESTORE_LEDS);
                 leds_status = 0;
-        }else {
-                if (leds_status == 0)
-                        leds_status = blink_mask;
-                else
-                        leds_status = 0;
-                (tty_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED, leds_status);
+        } else {
+                leds_status = (leds_status == 0) ? blink_mask : 0;
+                tty_driver->ops->ioctl(vc_cons[fg_console].d->port.tty, KDSETLED, leds_status);
         }
 
-        mod_timer(&timer, jiffies + BLINK_DELAY);
-        //printk(KERN_INFO "-=-=-Blink!-=-=-");
+        mod_timer(&timer, jiffies + msecs_to_jiffies(blink_delay_ms));
 }
 
 module_init(sys_init);
--- module5/practice3/practice3.mod.c
+++ module5/practice3/practice3.mod.c
@@ -18,79 +18,85 @@ __section(".gnu.linkonce.this_module") = {
 
 static const struct modversion_info ____versions[]
 __used __section("__versions") = {
-	{ 0x32feeafc, "mod_timer" },
-	{ 0xd272d446, "__x86_return_thunk" },
-	{ 0xe8213e80, "_printk" },
-	{ 0x90a48d82, "__ubsan_handle_out_of_bounds" },
-	{ 0xbd03ed67, "__ref_stack_chk_guard" },
-	{ 0x8e142c2e, "kstrtouint" },
 	{ 0xd272d446, "__stack_chk_fail" },
 	{ 0xdd6830c7, "sprintf" },
+	{ 0x7ec472ba, "fg_console" },
+	{ 0x1b0aeac3, "vc_cons" },
+	{ 0x5a844b26, "__x86_indirect_thunk_r12" },
+	{ 0x534ed5f3, "__msecs_to_jiffies" },
+	{ 0x058c185a, "jiffies" },
+	{ 0x32feeafc, "mod_timer" },
+	{ 0x90a48d82, "__ubsan_handle_out_of_bounds" },
 	{ 0xcc9483d5, "kernel_kobj" },
 	{ 0x9fec3e3f, "kobject_create_and_add" },
 	{ 0x3d3299c8, "sysfs_create_file_ns" },
 	{ 0x02f9bbf0, "timer_init_key" },
 	{ 0xa470b7e2, "add_timer" },
-	{ 0x2352b148, "timer_delete_sync" },
 	{ 0x45f86ffc, "kobject_put" },
+	{ 0x2352b148, "timer_delete_sync" },
+	{ 0x0f2f972e, "param_ops_uint" },
 	{ 0xd272d446, "__fentry__" },
-	{ 0x7ec472ba, "fg_console" },
-	{ 0x1b0aeac3, "vc_cons" },
-	{ 0x5a844b26, "__x86_indirect_thunk_r12" },
-	{ 0x058c185a, "jiffies" },
+	{ 0xbd03ed67, "__ref_stack_chk_guard" },
+	{ 0x8e142c2e, "kstrtouint" },
+	{ 0xe8213e80, "_printk" },
+	{ 0xd272d446, "__x86_return_thunk" },
 	{ 0xd954c786, "module_layout" },
 };
 
 static const u32 ____version_ext_crcs[]
 __used __section("__version_ext_crcs") = {
-	0x32feeafc,
-	0xd272d446,
-	0xe8213e80,
-	0x90a48d82,
-	0xbd03ed67,
-	0x8e142c2e,
 	0xd272d446,
 	0xdd6830c7,
+	0x7ec472ba,
+	0x1b0aeac3,
+	0x5a844b26,
+	0x534ed5f3,
+	0x058c185a,
+	0x32feeafc,
+	0x90a48d82,
 	0xcc9483d5,
 	0x9fec3e3f,
 	0x3d3299c8,
 	0x02f9bbf0,
 	0xa470b7e2,
-	0x2352b148,
 	0x45f86ffc,
+	0x2352b148,
+	0x0f2f972e,
 	0xd272d446,
-	0x7ec472ba,
-	0x1b0aeac3,
-	0x5a844b26,
-	0x058c185a,
+	0xbd03ed67,
+	0x8e142c2e,
+	0xe8213e80,
+	0xd272d446,
 	0xd954c786,
 };
 static const char ____version_ext_names[]
 __used __section("__version_ext_names") =
-	"mod_timer\0"
-	"__x86_return_thunk\0"
-	"_printk\0"
-	"__ubsan_handle_out_of_bounds\0"
-	"__ref_stack_chk_guard\0"
-	"kstrtouint\0"
 	"__stack_chk_fail\0"
 	"sprintf\0"
+	"fg_console\0"
+	"vc_cons\0"
+	"__x86_indirect_thunk_r12\0"
+	"__msecs_to_jiffies\0"
+	"jiffies\0"
+	"mod_timer\0"
+	"__ubsan_handle_out_of_bounds\0"
 	"kernel_kobj\0"
 	"kobject_create_and_add\0"
 	"sysfs_create_file_ns\0"
 	"timer_init_key\0"
 	"add_timer\0"
-	"timer_delete_sync\0"
 	"kobject_put\0"
+	"timer_delete_sync\0"
+	"param_ops_uint\0"
 	"__fentry__\0"
-	"fg_console\0"
-	"vc_cons\0"
-	"__x86_indirect_thunk_r12\0"
-	"jiffies\0"
+	"__ref_stack_chk_guard\0"
+	"kstrtouint\0"
+	"_printk\0"
+	"__x86_return_thunk\0"
 	"module_layout\0"
 ;
 
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "2403E7D3E053E3FCB8DCEC2");
+MODULE_INFO(srcversion, "5B8C8503D8AABE009852AC6");
